de6d36579c6fbdd8d1156c1f801e991df47a10fc
Tina Wu <tinawu0603@hotmail.com>
2020-03-27 19:17:37 -0400

    added styles and fixed course list

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { genericServer, nuid } from "./constants";
 
 @Injectable()
 export class CourseServiceClient {
     findAllCourses = () =>
-        fetch('http://wbdv-generic-server.herokuapp.com/api/jannunzi/courses')
+        fetch(genericServer + '/api/' + nuid + '/courses')
             .then(response => response.json())
+
+    findCourseById = (cid: string) =>
+        fetch(genericServer + '/api/' + nuid + '/courses/' + cid)
+            .then(response => response.json())
+
 }
