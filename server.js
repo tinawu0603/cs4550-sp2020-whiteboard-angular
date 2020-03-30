1e3b46da3c598ac10fa8c8f9951d3556c444c7c7
Tina Wu <tinawu0603@hotmail.com>
2020-03-30 14:23:14 -0400

    changed server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/cs4550-sp2020-whiteboard-angular'));
+app.use(express.static(__dirname + '/dist/cs4550-sp20-whiteboard-angular'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/cs4550-sp2020-whiteboard-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cs4550-sp20-whiteboard-angular/index.html'));
 });
 
 // Start the app by listening on the default heroku port
