fc82bf293d721aa022de7b39522e00b256386f25
Tina Wu <tinawu0603@hotmail.com>
2020-03-30 14:31:35 -0400

    use https for generic server

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,2 +1,2 @@
-export const genericServer: string = "http://wbdv-generic-server.herokuapp.com";
+export const genericServer: string = "https://wbdv-generic-server.herokuapp.com";
 export const nuid: string = "001280266";
