38691ba7f0fc8c2b39c425c22cfe97ca31e8177c
Tina Wu <tinawu0603@hotmail.com>
2020-03-30 13:25:35 -0400

    added module-list

--- src/app/module-list/module-list.component.ts
+++ src/app/module-list/module-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModuleServiceClient } from '../services/ModuleServiceClient';
 
 @Component({
   selector: 'app-module-list',
@@ -8,21 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ModuleListComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private service: ModuleServiceClient) { }
 
-  modules = [
-    { _id: '123', title: 'module 1' },
-    { _id: '234', title: 'module 2' },
-    { _id: '345', title: 'module 3' },
-  ]
+  modules = []
   courseId = ''
   moduleId = ''
+
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.courseId = params.cid;
       this.moduleId = params.mid;
-      fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses/${this.courseId}/modules`)
-        .then(response => response.json())
+      this.service.findModulesForCourse(this.courseId)
         .then(modules => this.modules = modules);
     });
   }
