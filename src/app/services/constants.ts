5094038f4f1e961cf03b37c468018f3ddb198971
Tina Wu <tinawu0603@hotmail.com>
2020-04-13 14:11:17 -0400

    add heroku node address

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,3 +1,4 @@
 export const genericServer: string = "https://wbdv-generic-server.herokuapp.com";
 export const nuid: string = "001280266";
-export const localhost: string = "http://localhost:3000"
+// export const localhost: string = "http://localhost:3000"
+export const localhost: string = "https://ancient-anchorage-36854.herokuapp.com";
